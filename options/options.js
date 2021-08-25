916c32b4b546323a4fa20a2303588f0702fe9bb7
Crowdbotics <team@crowdbotics.com>
2021-08-25 01:38:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "plain_night_30089Identifier",
+  url: "https://plain_night_30089Identifier.botics.co",
+  api: "https://plain_night_30089Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
